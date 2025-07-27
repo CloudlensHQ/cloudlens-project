0647ce138531dbfbce81bd900d0bbe572770ab18
amansingh0311 <786amanofficial@gmail.com>
2025-07-27 12:31:27 +0530

    Enhance Docker setup for PostgreSQL and application connectivity
    - Added PostgreSQL service to docker-compose with health checks and initialization script.
    - Updated server command to wait for database readiness before starting the application.
    - Included netcat installation in Dockerfile for improved database connectivity.
    - Configured environment variables for database connection in the server service.

---
+++ fastapi-backend/alembic/versions/0fda572cfd20_initial_migration.py
@@ -0,0 +1,24 @@
+"""Initial migration
+
+Revision ID: 0fda572cfd20
+Revises: f62d80e1f43d
+Create Date: 2025-07-27 06:12:10.186931
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0fda572cfd20'
+down_revision = 'f62d80e1f43d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass 
